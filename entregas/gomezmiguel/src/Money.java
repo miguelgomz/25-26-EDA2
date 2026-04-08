007b8191a84ab086c2b34540bb476138152e511b
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-08 13:39:10 +0200

    método resolver  da la ejecución, main crea la instancia de la clase Money

--- entregas/gomezmiguel/src/Money.java
+++ entregas/gomezmiguel/src/Money.java
@@ -8,4 +8,17 @@ public class Money {
         this.asignacion = new int[letrasUnicas.length()];
         this.digitoUsado = new boolean[10];
     }
+
+public void resolver() {
+        if (ejecutarRecursion(0)) {
+            imprimirResultado();
+        } else {
+            System.out.println("No se encontro solucion.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Money examen = new Money();
+        examen.resolver();
+    }
 }
