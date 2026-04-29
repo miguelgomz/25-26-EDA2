4d11778fac5e85fbe26f929ce7de310cbdab2250
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-29 13:48:47 +0200

    Insertion iterativo

---
+++ entregas/gomezmiguel/src/InsertionSort.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void ordenar(int[] array) {
+        System.out.println("INSERTION SORT ITERATIVO");
+        System.out.println("Array desordenado: " + Arrays.toString(array));
+        for (int i = 1; i
