b3d5bfcbb77b23d84ffab42b85bea8cd37490d4b
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-29 13:58:14 +0200

    agregue sort recursivo

--- entregas/gomezmiguel/src/InsertionSort.java
+++ entregas/gomezmiguel/src/InsertionSort.java
@@ -36,4 +36,47 @@ public class InsertionSort {
         }
         System.out.println("\nEstado Final: " + Arrays.toString(array) + "\n");
     }
+    public static void ordenar(int[] array, int n) {
+        if (nivelRecursion == 0) {
+            System.out.println("=== INSERTION SORT RECURSIVO ===");
+            System.out.println("Estado Inicial: " + Arrays.toString(array));
+        }
+        
+        System.out.println(getInd() + "Llamada recursiva (n = " + n + ")");
+        
+        if (n <= 1) {
+            System.out.println(getInd() + "CASO BASE: n <= 1. Subarray considerado ordenado.");
+            return;
+        }
+        
+        nivelRecursion++;
+        ordenar(array, n - 1);
+        nivelRecursion--;
+        
+        int ultimo = array[n - 1];
+        int j = n - 2;
+        
+        System.out.println("\n" + getInd() + "Retorno a n = " + n + " | Elemento a insertar: " + ultimo);
+        
+        while (j >= 0 && array[j] > ultimo) {
+            System.out.println(getInd() + "  Comparando array[" + j + "] (" + array[j] + ") > ultimo (" + ultimo + ") -> SI");
+            System.out.println(getInd() + "  Desplazando " + array[j] + " a la derecha.");
+            array[j + 1] = array[j];
+            j--;
+            System.out.println(getInd() + "  Estado temporal: " + Arrays.toString(array));
+        }
+        
+        if (j >= 0) {
+            System.out.println(getInd() + "  Comparando array[" + j + "] (" + array[j] + ") > ultimo (" + ultimo + ") -> NO (Posicion encontrada)");
+        } else {
+            System.out.println(getInd() + "  Se alcanzo el limite izquierdo del array.");
+        }
+        
+        array[j + 1] = ultimo;
+        System.out.println(getInd() + "Resultado en este nivel: " + Arrays.toString(array));
+        
+        if (nivelRecursion == 0) {
+            System.out.println("\nEstado Final: " + Arrays.toString(array) + "\n");
+        }
+    }
 }
